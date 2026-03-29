b4171d0291f5bc9a29e5978b37a4d9a2f5b003be
khaled.eid <khaled.eid1k1k@gmail.com>
2026-03-29 14:17:34 +0200

    Refactor file handling and add ProjectController for project-specific file management

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,8 +1,13 @@
+from signal import signal
+
 from fastapi import FastAPI, APIRouter , Depends,UploadFile,status
 from fastapi.responses import JSONResponse 
 from helpers.config import get_settings,Settings
-from controllers import DataController
-
+from controllers import DataController, ProjectController 
+import aiofiles
+import os
+from models import ResponseStatus
+ 
 data_router = APIRouter(
     prefix="/api/v1/data",
     tags=["api_v1", "data"],
@@ -15,5 +20,17 @@ async def upload_file(project_id: str, file: UploadFile, appsettings : Settings=
     is_valid , message = DataController().validate_upload_file(file)
     if not is_valid:
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"message": message})  
-    else:
-        return JSONResponse(status_code=status.HTTP_200_OK, content={"message": message})
+    # else:
+    #     return JSONResponse(status_code=status.HTTP_200_OK, content={"message": message})
+    
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
+    file_path = os.path.join(project_dir_path, file.filename)
+
+    async with aiofiles.open(file_path, 'wb') as out_file:
+        while chunk := await file.read(appsettings.FILE_DEFAULT_CHUNK_SIZE):  
+            await out_file.write(chunk)
+
+    return JSONResponse(content={
+        "signal": ResponseStatus.FILE_UPLOAD_SUCCESS.value,
+         
+    })  
